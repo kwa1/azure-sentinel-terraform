33c083460b790939eaa5f933aa78129fbfd0437c
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 04:24:17 +0000

    Create variables.tf

---
+++ modules/sentinel_role_assignments/variables.tf
@@ -0,0 +1,9 @@
+variable "user_object_id" {
+  description = "Azure AD Object ID of the user deploying Sentinel"
+  type        = string
+}
+
+variable "resource_group_id" {
+  description = "The ID of the Resource Group"
+  type        = string
+}
