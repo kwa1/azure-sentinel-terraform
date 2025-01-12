5da9d78d0b69e961adc388d7f95dc45d31d60d0a
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 05:04:10 +0000

    Update main.tf

--- modules/cloudwatch_agent/example/main.tf
+++ modules/cloudwatch_agent/example/main.tf
@@ -2,9 +2,9 @@ module "cloudwatch_agent" {
   source                = "./modules/cloudwatch_agent"
   ami_id                = "ami-0abcdef1234567890"
   instance_type         = "t2.micro"
-  instance_name_tag     = "CloudWatchAgentInstance"
-  config_source         = "s3"
+  instance_name_tag     = "MyCloudWatchInstance"
+  config_source         = "s3" # or "local"
   local_config_file_path = "./configs/amazon-cloudwatch-agent.json"
   s3_bucket_name        = "my-cloudwatch-configs"
-  s3_config_url         = "s3://my-cloudwatch-configs/amazon-cloudwatch-agent.json"
+  s3_config_key         = "amazon-cloudwatch-agent.json"
 }
