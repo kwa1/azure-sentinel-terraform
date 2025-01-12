9010b03d2ef0789f4b03f538158f5b054557e897
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 05:03:19 +0000

    Update variables.tf

--- modules/cloudwatch_agent/variables.tf
+++ modules/cloudwatch_agent/variables.tf
@@ -1,47 +1,38 @@
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "The AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
+  description = "The instance type for the EC2 instance"
   type        = string
 }
 
 variable "instance_name_tag" {
-  description = "Tag used to identify the instance"
+  description = "The tag name of the EC2 instance"
   type        = string
 }
 
 variable "config_source" {
-  description = "Source of the CloudWatch agent configuration (local or s3)"
+  description = "The source of the CloudWatch Agent configuration (either 's3' or 'local')"
   type        = string
+  default     = "s3"
 }
 
 variable "local_config_file_path" {
-  description = "Path to the local CloudWatch agent configuration file"
+  description = "The local path of the CloudWatch agent configuration file"
   type        = string
+  default     = "./configs/amazon-cloudwatch-agent.json"
 }
 
 variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for CloudWatch agent configuration"
+  description = "The S3 bucket name where the CloudWatch Agent configuration file is stored"
   type        = string
 }
 
-variable "s3_config_url" {
-  description = "S3 URL for the CloudWatch agent configuration file"
+variable "s3_config_key" {
+  description = "The S3 key for the CloudWatch Agent configuration file"
   type        = string
 }
 
-variable "agent_installed_check_command" {
-  description = "Command to check if CloudWatch agent is installed"
-  type        = string
-  default     = "command -v amazon-cloudwatch-agent"
-}
-
-variable "install_agent_command" {
-  description = "Command to install CloudWatch agent"
-  type        = string
-  default     = "yum install -y amazon-cloudwatch-agent"
-}
 
