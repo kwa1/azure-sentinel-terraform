d2c580671a68aa9368841415f1edef9f2a0ea363
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-12 04:49:21 +0000

    Update variables.tf

--- modules/cloudwatch_agent/variables.tf
+++ modules/cloudwatch_agent/variables.tf
@@ -1,45 +1,47 @@
 variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
+  description = "AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The type of EC2 instance"
+  description = "Instance type for the EC2 instance"
+  type        = string
+}
+
+variable "instance_name_tag" {
+  description = "Tag used to identify the instance"
   type        = string
 }
 
 variable "config_source" {
-  description = "Source of the CloudWatch Agent configuration file ('local' or 's3')"
+  description = "Source of the CloudWatch agent configuration (local or s3)"
   type        = string
-  default     = "s3"
 }
 
 variable "local_config_file_path" {
-  description = "Local file path for CloudWatch Agent configuration (if using local source)"
+  description = "Path to the local CloudWatch agent configuration file"
   type        = string
-  default     = ""
 }
 
 variable "s3_bucket_name" {
-  description = "S3 bucket name for CloudWatch Agent config (if using S3 source)"
+  description = "Name of the S3 bucket for CloudWatch agent configuration"
   type        = string
-  default     = ""
 }
 
 variable "s3_config_url" {
-  description = "URL of the S3 object containing the CloudWatch Agent config"
+  description = "S3 URL for the CloudWatch agent configuration file"
   type        = string
-  default     = ""
 }
 
 variable "agent_installed_check_command" {
-  description = "Command to check if the CloudWatch Agent is already installed"
+  description = "Command to check if CloudWatch agent is installed"
   type        = string
-  default     = "which amazon-cloudwatch-agent"
+  default     = "command -v amazon-cloudwatch-agent"
 }
 
 variable "install_agent_command" {
-  description = "Command to install the CloudWatch Agent if not installed"
+  description = "Command to install CloudWatch agent"
   type        = string
   default     = "yum install -y amazon-cloudwatch-agent"
 }
+
